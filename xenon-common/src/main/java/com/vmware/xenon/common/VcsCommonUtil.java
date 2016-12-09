89d2998f77007440ea58d718312ca8333bde853a
Nirdesh Shukla <nshukla@vmware.com>
2016-12-08 20:45:10 -0800

    correcting checkStyle error

--- xenon-common/src/main/java/com/vmware/xenon/common/VcsCommonUtil.java
+++ xenon-common/src/main/java/com/vmware/xenon/common/VcsCommonUtil.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright (c) 2014-2015 VMware, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, without warranties or
+ * conditions of any kind, EITHER EXPRESS OR IMPLIED.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
 package com.vmware.xenon.common;
 
 import java.io.BufferedReader;
@@ -7,9 +20,13 @@ import java.io.InputStreamReader;
 
 public class VcsCommonUtil {
 
+    private VcsCommonUtil() {
+
+    }
+
     /**
-     * 
-     * @param cmd 
+     *
+     * @param cmd
      *  e.g. private static final String CMD = "sh /tmp/scripts/get_datastores.sh";
      * @return
      * @throws IOException
@@ -24,7 +41,7 @@ public class VcsCommonUtil {
         String line;
         try {
 
-            br = new BufferedReader(new InputStreamReader(is));
+            br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
             while ((line = br.readLine()) != null) {
                 sb.append(line);
             }
